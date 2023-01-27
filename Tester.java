41ebe00981d75331017e3ff33d1a181e7d23aab9
Federico Urbani <urbanifederico5@gmail.com>
2023-01-27 22:17:05 +0100

    updated

---
+++ Tester.java
@@ -0,0 +1,8 @@
+public class Tester{
+
+    public static void main(String[] args) {
+        String a= "tup tuup tuuup tuuuup";
+        System.out.println(a.replaceAll("[u]{2,3}","x"));
+    }
+
+}
